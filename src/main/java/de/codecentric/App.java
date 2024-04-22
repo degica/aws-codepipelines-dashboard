674ca95a779e3ee358cdad26e3ecf76eac6d0c39
Muhammad Denaw <mdenaw@degica.com>
2024-04-22 14:13:06 +0700

    [AWS][REGION] Update from EU_CENTRAL_1 to AP_NORTHEAST_1

--- src/main/java/de/codecentric/App.java
+++ src/main/java/de/codecentric/App.java
@@ -16,6 +16,6 @@ public class App {
 
     @Bean
     public AWSCodePipeline getAwsCodePipelineClient() {
-        return AWSCodePipelineClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
+        return AWSCodePipelineClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_1).build();
     }
 }
